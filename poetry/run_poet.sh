83d3d3a96882ea342e1e8943a2a600663053c087
mgrankin <mv.grankin@gmail.com>
2019-10-01 06:27:07 +0300

    dry dataset

--- poetry/run_poet.sh
+++ poetry/run_poet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-eval "$(conda shell.bash hook)"
+source ~/.bashrc
 conda activate gpt
 cd /home/u/ru_transformers/poetry
 python scheduler.py
--- poetry/train_poet.sh
+++ poetry/train_poet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-eval "$(conda shell.bash hook)"
+source ~/.bashrc
 conda activate gpt
 export CUDA_VISIBLE_DEVICES=0
 cd /home/u/ru_transformers/poetry/
@@ -12,7 +12,7 @@ for i in {1..40}; do
         --model_type=gpt2 \
         --model_name_or_path=poetry/output_poet \
         --do_train \
-        --train_data_file=data/poetry.txt \
+        --train_data_file=data/poetry_dry.txt \
         --per_gpu_train_batch_size=3 \
         --save_steps=10000 \
         --logging_steps=1 \
@@ -24,6 +24,7 @@ for i in {1..40}; do
         --tokenizer_class SPEncoder \
         --tokenizer_name bpe/m50.model \
         --lr_decay
+    sleep 1
 done
 
 # 0 2 * * * /home/u/ru_transformers/poetry/train_poet.sh
