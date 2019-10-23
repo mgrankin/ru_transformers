315e2eb0df092e8756502a5d8527fea656a7281b
mgrankin <mv.grankin@gmail.com>
2019-10-23 14:11:44 +0300

    warmup

--- tpu_lm_finetuning.py
+++ tpu_lm_finetuning.py
@@ -312,7 +312,7 @@ def train(args, train_dataset, model, tokenizer):
         ]
     # Scale learning rate to num cores
     optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate * xm.xrt_world_size(), eps=args.adam_epsilon)
-    warmup_steps = args.warmup_samples // args.train_batch_size
+    warmup_steps = args.warmup_samples // (args.train_batch_size * xm.xrt_world_size())
     if args.lr_decay:
         scheduler = WarmupLinearSchedule(optimizer, warmup_steps=warmup_steps, t_total=t_total)
     else:
