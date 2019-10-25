872ef7ad67504a8ea143e0675ebcdd42fc0954d3
mgrankin <mv.grankin@gmail.com>
2019-10-25 11:00:35 +0300

    debug

--- tpu_lm_finetuning.py
+++ tpu_lm_finetuning.py
@@ -403,7 +403,8 @@ def train(args, train_dataset, model, tokenizer):
 
                     if args.save_steps > 0 and global_step % args.save_steps == 0:
                         save_state(args, model, tokenizer, global_step)
-
+                    xm.mark_step()
+                    
                 if args.max_steps > 0 and step > args.max_steps:
                     epoch_iterator.close()
                     break
