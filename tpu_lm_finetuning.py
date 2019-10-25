11ace0f42312c43055e4f12f3e7e6e47af2e90df
mgrankin <mv.grankin@gmail.com>
2019-10-25 14:19:21 +0300

    debug

--- tpu_lm_finetuning.py
+++ tpu_lm_finetuning.py
@@ -473,6 +473,7 @@ def evaluate(args, model, tokenizer, prefix=""):
     model.eval()
     outputs = []
 
+    xm.mark_step()
     #with torch.no_grad():
         #for batch in tqdm(eval_dataloader.per_device_loader(args.device), desc="Evaluating"):
     for batch in eval_dataloader.per_device_loader(args.device):
