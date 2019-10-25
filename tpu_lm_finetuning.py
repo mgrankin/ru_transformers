44268e0e04751b43e736c0896713ef0d38863857
mgrankin <mv.grankin@gmail.com>
2019-10-25 13:25:25 +0300

    debug

--- tpu_lm_finetuning.py
+++ tpu_lm_finetuning.py
@@ -398,22 +398,22 @@ def train(args, train_dataset, model, tokenizer):
                 print(f'{xm.get_ordinal()} step')
                 optimizer.zero_grad()
                 xm.mark_step()
-                    '''
-                    scheduler.step()                      
-                    global_step += 1
-                    tracker.add(args.train_batch_size)
+                '''
+                scheduler.step()                      
+                global_step += 1
+                tracker.add(args.train_batch_size)
 
-                    if args.logging_steps > 0 and global_step % args.logging_steps == 0:
-                        ls = loss.item() # weird. if you call loss.item() only in one process, the whole thing hangs. So call on every and log in one.
-                        moving_loss.add(ls)
-                        if args.local_rank in [-1, 0]:
-                            #tb_writer.add_scalar('lr', scheduler.get_last_lr()[0], global_step)
-                            logger.info(f"Tracker rate {tracker.rate():.2f}, Global rate {tracker.global_rate():.2f}")
-                            logger.info(f"Moving loss {moving_loss.loss:.2f}, perplexity {torch.exp(torch.tensor(moving_loss.loss)):.2f}")
+                if args.logging_steps > 0 and global_step % args.logging_steps == 0:
+                    ls = loss.item() # weird. if you call loss.item() only in one process, the whole thing hangs. So call on every and log in one.
+                    moving_loss.add(ls)
+                    if args.local_rank in [-1, 0]:
+                        #tb_writer.add_scalar('lr', scheduler.get_last_lr()[0], global_step)
+                        logger.info(f"Tracker rate {tracker.rate():.2f}, Global rate {tracker.global_rate():.2f}")
+                        logger.info(f"Moving loss {moving_loss.loss:.2f}, perplexity {torch.exp(torch.tensor(moving_loss.loss)):.2f}")
 
-                    if args.save_steps > 0 and global_step % args.save_steps == 0:
-                        save_state(args, model, tokenizer, global_step)
-                    '''
+                if args.save_steps > 0 and global_step % args.save_steps == 0:
+                    save_state(args, model, tokenizer, global_step)
+                '''
                 if step > 10:
                     epoch_iterator.close()
                     break
