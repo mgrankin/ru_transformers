b5b2f76225e734072b7d56674983c29a543077e0
mgrankin <mv.grankin@gmail.com>
2019-10-25 12:31:29 +0300

    debug

--- tpu_lm_finetuning.py
+++ tpu_lm_finetuning.py
@@ -385,7 +385,7 @@ def train(args, train_dataset, model, tokenizer):
                     loss = loss / args.gradient_accumulation_steps
                 
                 loss.backward()
-                print(f'{args.device} {backward}')
+                print(f'{args.device} backward')
 
                 if (step + 1) % args.gradient_accumulation_steps == 0:
                     torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
