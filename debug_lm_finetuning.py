4511d94230c2e4b29ae8fde713ad40566a879ddb
mgrankin <mv.grankin@gmail.com>
2019-10-28 11:24:49 +0300

    debug

--- debug_lm_finetuning.py
+++ debug_lm_finetuning.py
@@ -170,7 +170,7 @@ class TextDataset(Dataset):
         # consider it as a data augmentation technique. 
         # In case of TPU, you need to make sure initial random seed is the same for each process or TPU will freeze because of 
         # different datasets.
-        files = files[:10]
+        files = files[:100]
 
         self.examples = []
         
